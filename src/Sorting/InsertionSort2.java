3189a438979b6678cebcb1aa2a0d9f88336f250b
Joe Gallego <josephjgallego@gmail.com>
2020-04-07 22:52:54 -0700

    adding examples

--- src/Sorting/InsertionSort2.java
+++ src/Sorting/InsertionSort2.java
@@ -1,4 +1,23 @@
 package Sorting;
 
 public class InsertionSort2 {
+
+
+
+    public static void main(String[] args) {
+
+        int[] array = new int[20];
+
+        for (int i = 1; i < array.length; i++){
+            int j, v = array[i];
+            for (j = i - 1; i >= 0; j--){
+                if (array[j] <= v) {
+                    break;
+                }
+            }
+            
+        }
+
+    }
+
 }
