5bfab29786ebe2511ada253c144f770ca2c39250
Joe Gallego <josephjgallego@gmail.com>
2020-04-07 22:15:18 -0700

    adding examples

---
+++ src/Sorting/Sorts.java
@@ -0,0 +1,15 @@
+package Sorting;
+
+public class Sorts {
+    public static void main(String[] args) {
+
+    }
+}
+
+// out of place sort - all
+// in place sort - one
+
+// stable sort - order of duplicates unchanged
+// unstable sort - changed
+
+// complexity: worst, average, best 
