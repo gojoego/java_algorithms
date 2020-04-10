91096ba197b9246632067d1546255bb1150b1d99
Joe Gallego <josephjgallego@gmail.com>
2020-04-10 09:03:53 -0700

    updating

--- src/Sorting/BubbleSort3.java
+++ src/Sorting/BubbleSort3.java
@@ -1,4 +1,33 @@
 package Sorting;
 
+import java.util.Arrays;
+
 public class BubbleSort3 {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{23, 12, 1, -4, 400, 55, -22, 168};
+
+        System.out.println("unsorted: " + Arrays.toString(arr));
+
+        bubbleSort(arr);
+
+        System.out.println("unsorted: " + Arrays.toString(arr));
+
+    }
+
+    private static void bubbleSort(int[] arr) {
+
+        for (int i = 1; i < arr.length; i++) {
+
+            for (int j = 1; j < arr.length - 1; j++) {
+
+                if (arr[j = 1] > arr[j]) {
+                    int t = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = t;
+                }
+            }
+        }
+    }
 }
