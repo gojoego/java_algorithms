ec6e507f18bff076d83af70c34f7e9a03752feb8
Joe Gallego <josephjgallego@gmail.com>
2020-04-12 13:07:23 -0700

    adding practice

--- src/Sorting/QuickSort1.java
+++ src/Sorting/QuickSort1.java
@@ -18,8 +18,25 @@ public class QuickSort1<E> {
         array[from] = array[to];
         array[to] = temp;
     }
-    
-    private void quickSort(int i, int i1) {
+
+    public void quickSort(int from, int to) {
+
+        if (from >= to){
+            return;
+        }
+
+        E value = array[to];
+        int counter = from;
+
+//        for (int i = from; i < to; i++){
+//            if (((Comparable<E>)array[i].compareTo(value)) <= 0){
+//                swap(i, counter);
+//                counter++;
+//            }
+//        }
+        swap(counter, to);
+        quickSort(from, counter - 1);
+        quickSort(counter + 1, to);
     }
 
 
