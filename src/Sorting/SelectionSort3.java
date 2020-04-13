123005621e2b3fa1204c8cc12cd72ea68e175df1
Joe Gallego <josephjgallego@gmail.com>
2020-04-12 21:35:15 -0700

    adding practice

--- src/Sorting/SelectionSort3.java
+++ src/Sorting/SelectionSort3.java
@@ -7,40 +7,41 @@ public class SelectionSort3 {
 
     public static void main(String[] args) {
 
-        SelectionSort3 ob = new SelectionSort3();
+        // create object of class to run sorting method on array
+        SelectionSort3 object = new SelectionSort3();
         int[] array = new int[]{46, 7, 36, 21, 20, 31, 99};
-        System.out.println("unsorted: " + Arrays.toString(array));
-        ob.sort(array);
-        System.out.println("sorted: " + Arrays.toString(array));
+        System.out.println("unsorted: ");
+        printArray(array);
+        object.sort(array);
+        System.out.println();
+        System.out.println("sorted: ");
+        printArray(array);
 
     }
-
 
     public void sort(int[] array) {
         int n = array.length;
 
-        // One by one move boundary of unsorted subarray
+        // use for loop to iterate through each index of array
         for (int i = 0; i < n - 1; i++) {
-            // Find the minimum element in unsorted array
-            int min_idx = i;
+            // lowest value index
+            int low = i;
             for (int j = i + 1; j < n; j++)
-                if (array[j] < array[min_idx])
-                    min_idx = j;
+                if (array[j] < array[low])
+                    low = j;
 
-            // Swap the found minimum element with the first
-            // element
-            int temp = array[min_idx];
-            array[min_idx] = array[i];
+            // swapping lowest index with first index
+            int temp = array[low];
+            array[low] = array[i];
             array[i] = temp;
         }
     }
 
-    // Prints the array
-    void printArray(int arr[]) {
-        int n = arr.length;
+    // for loop to print array
+    public static void printArray(int array[]) {
+        int n = array.length;
         for (int i = 0; i < n; ++i)
-            System.out.print(arr[i] + " ");
-        System.out.println();
+            System.out.print(array[i] + " ");
     }
 
 }
